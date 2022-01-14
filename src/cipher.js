55aa5144c2c722c22f3416b373b05f77d8fb336a
VANE VERA <vanessavv_15@hotmail.com>
2022-01-13 23:16:44 -0600

    prueba 2 de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -4,8 +4,8 @@
 //export default cipher;
 //crerando variable cesar o la funcion anonima que regresara a 
 //una funcion con 3 parametros
-let cesar = cesar || (function(){
- let proceso=function (txt,desp,action) {
+var cesar = cesar || (function(){
+ let proceso=function (texto,desp,action) {
   let replace = (function () {
   //matriz del alfabeto 
   let alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l',
@@ -14,7 +14,7 @@ let cesar = cesar || (function(){
 // obtener posicion de la llave privada
 return function (c) {
   //vamos  saber la posicion
-let i =alfabeto.indexOf(c.toLowerCase);
+let i =alfabeto.indexOf(c.toLowerCase() );
 //ddonde estamos en la matriz y como se recorrera y que pasa cuando llegue al final 
 if (i!=-1){
 //primero tenemos la posicion de mi desplazzamiento
@@ -22,34 +22,37 @@ let pos = i;
 //que decifrare
 if (action) {
   //cifrar para adelante
-pos +=desp;
+pos += desp;
 //como se movera
-pos-=(pos >= l )?l:0;
-else{
+pos-= (pos >= l )?l:0;
+}else {
   //descifrar en reversa
   pos -= desp;
   pos+= (pos < 0)?l:0;
 }
-return alfabeto [pos];
+return texto   [pos];
+
 }
 return c;
 };
 
- })();
+})();
  //tenermos que ver que el texto este acode al abc
- let re = (/([a-z]))/ig);
+ let re = (/([a-z])/ig);
 //una funcion que se encargue de intercambiar
-return String(txt).replace(re, function (match) {
+return String(texto).replace(re, function (match) {
   return replace (match);
+});
+
+ };
   return{
-    encode : function (txt,desp) {
-      return proceso (txt,desp,true);
+    encode : function (texto,desp) {
+      return proceso (texto,desp,true);
     },
-decode :function (txt,desp) {
-  return proceso(txt,desp,false);
+      decode :function (texto,desp) {
+  return proceso(texto,desp,false);
 }
 };
-
 })();
 //funcion cifrado
 function cifrar () {
@@ -57,8 +60,7 @@ document.getElementById("resultado").innerHTML =
 cesar.encode(document.getElementById("cadena").value,3);
 }
 //funcios descifrar
-function desifrar() {
+function descifrar() {
   document.getElementById("resultado").innerHTML =
   cesar.decode(document.getElementById("cadena").value,3);
-  
 }
