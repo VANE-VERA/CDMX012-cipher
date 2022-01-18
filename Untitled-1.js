43b721538af67c89430453c47d295829efcd0c33
VANE VERA <vanessavv_15@hotmail.com>
2022-01-18 12:40:55 -0600

    jv prueba test incluye eslintrc

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+pos -= (pos >= l) ? l : 0;
+
+if (pos >= 1) {
+    pos -= l;
+}
+else {
+    pos -= 0;
+}
--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,21 @@
 //const cipher = {
-// ...
-//}; 
-//una funcion con 3 parametros
- var cipher = cipher || (function () {
+
+var cipher = cipher || (function () {
   let proceso = function (txt, desp, action) {
     let replace = (function () {
       //matriz del alfabeto 
       let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
         'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
       let l = abc.length;
-      // obtener posicion de la llave privada
-      return function (c) {
-        //vamos  saber la posicion
-        let i = abc.indexOf(c.toUpperCase());
+      // mi funcion para recorrer mensaje por letra
+      return function (recorrerT) {
+        //el numero de posiicon de mi letra en abc
+        let i = abc.indexOf(recorrerT.toUpperCase());
         //ddonde estamos en la matriz y como se recorrera y que pasa cuando llegue al final 
         if (i != -1) {
           //primero tenemos la posicion de mi desplazzamiento
           let pos = i;
-          //que decifrare
+          //quieres cifrar(true) o descifrar(false)
           if (action) {
             //cifrar para adelante
             pos += desp;
@@ -31,11 +29,10 @@
           return abc[pos];
 
         }
-        return c;
+        return recorrerT;
       };
 
     })();
-    //tenermos que ver que el texto este acode al abc
     let re = (/([A-Z])/ig);
     //una funcion que se encargue de intercambiar
     return String(txt).replace(re, function (match) {
@@ -52,22 +49,19 @@
     }
   };
 })();
-//export default cipher;
-
-//funcion cifrado
 
 function cifrar() {
 
   document.getElementById("resultado").innerHTML =
     cipher.encode(document.getElementById("cadena").value,
-    //document.getElementById("offset").value);
-    parseInt(document.getElementById("offset").value));
+      parseInt(document.getElementById("offset").value));
 }
-//funcios descifrar
+
 function descifrar() {
   document.getElementById("resultado").innerHTML =
     cipher.decode(document.getElementById("cadena").value,
-    // document.getElementById("offset").value);
-     parseInt(document.getElementById("offset").value));
+      parseInt(document.getElementById("offset").value));
 
 }
+export default cipher;
+
